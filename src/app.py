787c8f5b745a7a97c7caf467f85b8c054cdcb748
cayopolo <c.nataliopolo13@gmail.com>
2025-12-19 17:22:21 +0000

    changes for deployment to python anywhere

--- src/app.py
+++ src/app.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 from . import routes
 
+
 app = Flask(__name__, template_folder="templates")
 routes.init_app(app)
-
-if __name__ == "__main__":
-    app.run(debug=True)  # noqa: S201
